e8ae8749b7f25ba4432bcb0b5660d3cbcc9fc5ad
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-01-21 04:07:19 -0500

    setup project

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
 const map = document.getElementById("mapCanvas");
-const ctx = map.getAnimationContext()
+map.height = window.innerHeight;
+map.width = 200;
+
+const ctx = map.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
