fda60ddc974419c004b9aa55364454e62ff59797
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-01-21 19:08:51 -0500

    added rotation logic for car

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
 const map = document.getElementById("mapCanvas");
-map.height = window.innerHeight;
 map.width = 200;
 
 const ctx = map.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
+
+animate();
+
+function animate() {
+    car.update();
+
+    map.height = window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
